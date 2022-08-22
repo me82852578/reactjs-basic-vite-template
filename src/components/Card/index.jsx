0b10f385dce56e181d26d6771bfb65fcbb36c291
Ericsen Tsai <bommer95175@gmail.com>
2022-08-22 10:13:48 +0800

    build: modify config files and file mocking files

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import PropTypes from 'prop-types'
 import CardHeader from './CardHeader'
 import styles from './index.module.scss'
--- src/components/HiComponent.jsx
+++ src/components/HiComponent.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 function HiComponent() {
   return <div>Hello</div>
 }
